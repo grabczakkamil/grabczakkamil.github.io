2a822e2e3dc78c78877c103c524a7076b0527f38
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-07 22:32:32 +0200

    zadanie8728-pobierz-dane-programisty-koniec-działa

---
+++ zadania-domowe/zadanie8728-pobierz-dane-programisty/js/index.js
@@ -0,0 +1,16 @@
+'use strict';
+$(function(){
+    
+    $("button").click(function () {
+
+
+    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+
+
+        var str = "imie: " + data.imie + "; " + "nazwisko: " + data.nazwisko + "; " + "zawód: " + data.zawod + "; " + "firma: " + data.firma + "; ";
+            $("#dane-programisty").html(str);
+
+        });
+    });
+    
+});
