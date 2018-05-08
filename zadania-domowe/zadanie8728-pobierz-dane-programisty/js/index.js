c2001fbc4f295862111e2cac6db10990467a7a66
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-08 21:27:19 +0200

    zadanie8728- poprawka - stworzono div za pomocą jquery

--- zadania-domowe/zadanie8728-pobierz-dane-programisty/js/index.js
+++ zadania-domowe/zadanie8728-pobierz-dane-programisty/js/index.js
@@ -1,16 +1,17 @@
 'use strict';
-$(function(){
-    
+$(function () {
+
     $("button").click(function () {
 
+        $('body').append('<div id="dane-programisty"></div>');
 
-    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
 
 
-        var str = "imie: " + data.imie + "; " + "nazwisko: " + data.nazwisko + "; " + "zawód: " + data.zawod + "; " + "firma: " + data.firma + "; ";
+            var str = "imie: " + data.imie + "; " + "nazwisko: " + data.nazwisko + "; " + "zawód: " + data.zawod + "; " + "firma: " + data.firma + "; ";
             $("#dane-programisty").html(str);
 
         });
     });
-    
+
 });
