b818d6ef65578b9d16149e37b87d12fb94b38dfb
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-21 23:28:26 +0200

    zadanie8786-zmienia na czerwono

--- zadania-domowe/zadanie8786-wyplaty-premie/js/index.js
+++ zadania-domowe/zadanie8786-wyplaty-premie/js/index.js
@@ -1,16 +1,43 @@
 'use strict';
 
 //function obliczWyplate() {
+    
      
-//    for (var i = 3; i<=23; i++) {
-//        
-//        var pracownik = document.getElementById("pracownicy").childNodes[5];
+    for (var i=0; i<=19; i++) {
+        var stawka = document.getElementsByClassName("stawka")[i].value;
         
-//        var stawka = parseInt(document.getElementsByClassName("wyplata").value);
+        var godziny = document.getElementsByClassName("czas")[i].value;
         
-        console.log(document.getElementById("pracownicy"));
+//        obliczanie wypłaty lub wypłaty z nadgodzinami
+        
+        var wyplata = 0;
+        
+        if (godziny>160) {
+            
+        var podwojnaStawka = 2 * stawka;
+        var nadgodziny = godziny - 160;
+        var podstawa = stawka * godziny;
+        var premia = podwojnaStawka * nadgodziny;
+        var wyplata = podstawa + premia;
+        }
+        
+        else {
+        
+        var wyplata = stawka * godziny;
+        }
+        
+//        oznaczanie pracowników któzy przepracowali mniej niż 100 godzin
+        
+        if (godziny < 100) {
+            document.getElementsByClassName("pracownik")[i].style.background="red";
+        }
+        
+//        wstawianie wartości wynagrodzenia do pola span.wyplata
+        document.getElementsByClassName("wyplata")[i].innerHTML=wyplata;
+
+
+    }
+
+
+        
 //    }
-//    
-////}
-//
-//obliczWyplate();
